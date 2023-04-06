a1e5b8b352d75105bf7c681b2c7b2eb39528f243
ADNTRe QWATTe <118337098+KU55555@users.noreply.github.com>
2023-04-06 11:59:09 +0300

    Update Program.cs

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ConsoleApp5
+namespace ConsoleApp5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("koolid");
+
+            Dictionary<int, string> koolid = new Dictionary<int, string>
+
+{
+
+    {1, "Jakob Westholmi Gümnaasium" },
+
+    {2, "Kadrioru Saksa Gümnaasium" },
+
+    {3, "Gustav Adolfi Gümnaasium"}
+
+};
+
+            foreach (KeyValuePair<int, string> pair in koolid)
+
+            {
+
+                Console.WriteLine("{1} on {0}", pair.Value, pair.Key);
+
+            }
+
+
         }
+
     }
 }
